64fdf88de5bcb05e25fa7e7f7217be4fa4d526e0
tobi8068 <hana.dev22@gmail.com>
2025-02-06 14:22:03 -0500

    fixed: amm

--- src/swap_functions/new_signed_and_send.rs
+++ src/swap_functions/new_signed_and_send.rs
@@ -12,24 +12,24 @@ use std::str::FromStr;
 pub async fn new_signed_and_send(
     client: &RpcClient,
     keypair: &Keypair,
-    instructions: Vec<Instruction>,
+    mut instructions: Vec<Instruction>,
     use_jito: bool,
 ) -> Result<Vec<String>> {
-    // let unit_limit = get_unit_limit();
-    // let unit_price = get_unit_price();
-    // // If not using Jito, manually set the compute unit price and limit
-    // if !use_jito {
-    //     let modify_compute_units =
-    //         solana_sdk::compute_budget::ComputeBudgetInstruction::set_compute_unit_limit(
-    //             unit_limit,
-    //         );
-    //     let add_priority_fee =
-    //         solana_sdk::compute_budget::ComputeBudgetInstruction::set_compute_unit_price(
-    //             unit_price,
-    //         );
-    //     instructions.insert(0, modify_compute_units);
-    //     instructions.insert(1, add_priority_fee);
-    // }
+    let unit_limit = get_unit_limit();
+    let unit_price = get_unit_price();
+    // If not using Jito, manually set the compute unit price and limit
+    if !use_jito {
+        let modify_compute_units =
+            solana_sdk::compute_budget::ComputeBudgetInstruction::set_compute_unit_limit(
+                unit_limit,
+            );
+        let add_priority_fee =
+            solana_sdk::compute_budget::ComputeBudgetInstruction::set_compute_unit_price(
+                unit_price,
+            );
+        instructions.insert(0, modify_compute_units);
+        instructions.insert(1, add_priority_fee);
+    }
     // send init tx
     let recent_blockhash = client.get_latest_blockhash()?;
     let txn = Transaction::new_signed_with_payer(
@@ -69,9 +69,9 @@ pub fn get_unit_price() -> u64 {
         .unwrap_or(20000)
 }
 
-// pub fn get_unit_limit() -> u32 {
-//     env::var("UNIT_LIMIT")
-//         .ok()
-//         .and_then(|v| u32::from_str(&v).ok())
-//         .unwrap_or(200_000)
-// }
+pub fn get_unit_limit() -> u32 {
+    env::var("UNIT_LIMIT")
+        .ok()
+        .and_then(|v| u32::from_str(&v).ok())
+        .unwrap_or(200_000)
+}
